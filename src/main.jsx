e6d578a9c1e86f4ec87e40c0fe5976cf4b72e8c2
Adeyinka Adebayo A <adeyinkaadebayo032@gmail.com>
2025-02-27 16:30:31 +0100

    e

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Main_task from './components/Task_management/Main_task.jsx'
 import Main_team from './components/Team_collaboration/Main_team.jsx'
@@ -15,36 +12,20 @@ import Sign_up from './components/Sign_up/Sign_up.jsx'
 import Error from './components/Error.jsx';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "Main_task",
-    element: <Main_task />,
-  },
-  {
-    path: "Main_team",
-    element: <Main_team />,
-  },
-  {
-    path: "Main_support",
-    element: <Main_support />,
-  },
-  {
-    path: "Sign_up",
-    element: <Sign_up />,
-  },
-  {
-    path: "*",
-    element: <Error />,
-  },
-])
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
-    <RouterProvider router={router}  />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Main_task" element={<Main_task />} />
+        <Route path="/Main_team" element={<Main_team />} />
+        <Route path="/Main_support" element={<Main_support />} />
+        <Route path="/Sign_up" element={<Sign_up />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
